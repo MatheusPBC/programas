83b59e2206878cf3e130c0178bfbd94c65e8cc27
MatheusPBC <matheuspbrackmannc@gmail.com>
2020-10-26 11:42:58 -0300

    rich text editor

---
+++ Java/script.js
@@ -0,0 +1,24 @@
+//string
+var texto = "variavel de javascript"
+
+//number
+var numero = 33
+
+//boolean 
+var teste = true
+
+alert(texto)
+
+document.write(texto)
+
+console.log()
+
+document.write("<h1>Ola" + numero + ", mundo</h1>")
+
+var nome = prompt("digite algo")
+
+//null 
+var teste = null
+
+//undefined
+var teste2 = undefined //or var teste2
