a2a28be0e24d87462a2c153408a16fb48faba0f9
MatheusPBC <matheuspbrackmannc@gmail.com>
2020-10-27 03:23:55 -0300

    Commit Message

---
+++ JavaScript/script.js
@@ -0,0 +1,12 @@
+function calcularArea(largura, comprimento) {
+    var area = largura * comprimento
+    return area
+    
+}
+
+var largura = prompt('')
+var comprimento = prompt('')
+
+var area = calcularArea(largura, comprimento)
+
+document.write(area)
